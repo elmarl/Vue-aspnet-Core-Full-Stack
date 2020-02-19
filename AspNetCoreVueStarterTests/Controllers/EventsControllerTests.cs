1723743a660bff72bc4cb9000ee5642fa1020be3
Elmar <elmar.laigna@gmail.com>
2020-02-19 19:00:00 +0200

    added files to the repo

---
+++ AspNetCoreVueStarterTests/Controllers/EventsControllerTests.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using AspNetCoreVueStarter.Service;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreVueStarter.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreVueStarter.Controllers.Tests
+{
+    
+    [TestClass()]
+    public class EventsControllerTests
+    {
+        private Mock<IEventService> _mockService;
+
+        [TestMethod()]
+        public void GetEventModelTest()
+        {
+            // Arrange
+            _mockService = new Mock<IEventService>();
+            _mockService.Setup(repo => repo.GetEvents())
+            .Returns(GetTestSessions());
+            var controller = new EventsController(_mockService.Object);
+
+            // Act
+            var result = controller.GetEventModel();
+        
+            // Assert
+            Assert.IsInstanceOfType(result.Value , typeof(IEnumerable<EventModel>));
+        }
+        [TestMethod]
+        public void GetEventModelTest_EmptyRepo()
+        {
+            // Arrange
+            _mockService = new Mock<IEventService>();
+            _mockService.Setup(repo => repo.GetEvents())
+            .Returns(GetEmptySession());
+            var controller = new EventsController(_mockService.Object);
+
+            // Act
+            var result = controller.GetEventModel();
+
+            // Assert
+            Assert.IsInstanceOfType(result.Value, typeof(IEnumerable<EventModel>));
+        }
+        [TestMethod]
+        public void GetEventModelTest_NullIdRepo()
+        {
+            // Arrange
+            int id = 0;
+            _mockService = new Mock<IEventService>();
+            _mockService.Setup(repo => repo.GetEvent(id))
+            .Returns(GetNullSession());
+            var controller = new EventsController(_mockService.Object);
+
+            // Act
+            var result = controller.GetEventModel(id);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
+        }
+        // Test invalid modelstate for events
+        [TestMethod]
+        public void PostEventModelTest_addMissingNameEventToRepo()
+        {
+            // Arrange
+            _mockService = new Mock<IEventService>();
+            var controller = new EventsController(_mockService.Object);
+            controller.ModelState.AddModelError("EventName","required");
+
+            // Act
+            var testEvent = new EventModel() { EventDate = DateTime.Now, Location = "testlocation", Details = "testdetails", };
+            var result = controller.PostEventModel(testEvent);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(BadRequestResult));
+        }
+
+        // Test case for deleting events
+        [TestMethod]
+        public void DeleteEventModel_DeleteNonExistantEvent()
+        {
+            // Arrange
+            int id = 0;
+            _mockService = new Mock<IEventService>();
+            var controller = new EventsController(_mockService.Object);
+            controller.ModelState.AddModelError("EventDate", "must be in the future");
+
+            // Act
+            var result = controller.DeleteEventModel(id);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
+        }
+        // Test invalid modelstate for events
+        [TestMethod]
+        public void PostParticipantModelTest_InvalidModelstate()
+        {
+            // Arrange
+            _mockService = new Mock<IEventService>();
+            var controller = new EventsController(_mockService.Object);
+            controller.ModelState.AddModelError("Firstname", "required");
+
+            // Act
+            var pModel = new ParticipantModel() { Firstname="test", Familyname="test2", Idcode="123", ParticipantType="person", Paymentmethod="kaardimakse", };
+            var result = controller.PostParticipantModel(0, pModel);
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(BadRequestResult));
+        }
+        // 
+        private EventModel GetNullSession()
+        {
+            return null;
+        }
+
+        private List<EventModel> GetEmptySession()
+        {
+            return new List<EventModel>();
+        }
+
+        private List<EventModel> GetTestSessions()
+        {
+            var sessions = new List<EventModel>();
+            sessions.Add(new EventModel()
+            {
+                Eventid = 1,
+                EventName = "Test event name future",
+                EventDate = new DateTime().AddDays(5),
+                Location = "Test location",
+                Details = "Test details",
+            });
+            sessions.Add(new EventModel()
+            {
+                Eventid = 1,
+                EventName = "Test event name past",
+                EventDate = new DateTime().AddDays(100),
+                Location = "Test location 2",
+            });
+            return sessions;
+        }
+    }
+}
---
+++ AspNetCoreVueStarterTests/Models/ModelTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using AspNetCoreVueStarter.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspNetCoreVueStarterTests.Models
+{
+    [TestClass()]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void EventModel_WorkingCaseValidation()
+        {
+            var sut = new EventModel() {EventName="name", EventDate=DateTime.Today.AddDays(1), Location="loc", };
+            // Set some properties here
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            // Assert here
+            Assert.AreEqual(true, isModelStateValid);
+        }
+        // Validate custom date attribute
+        [TestMethod]
+        public void EventModel_CustomDateAttributeValidation()
+        {
+            var sut = new EventModel() { EventName = "name", EventDate = DateTime.Today.AddDays(-1), Location = "loc", };
+            // Set some properties here
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            // Assert here
+            Assert.AreEqual(false, isModelStateValid);
+        }
+        // Validate details length
+        [TestMethod]
+        public void EventModel_DetailsLengthValidation()
+        {
+            string details = new string('*', 1001);
+            var sut = new EventModel() { EventName = "name", EventDate = DateTime.Today.AddDays(1), Location = "loc", Details = details, };
+            // Set some properties here
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            // Assert here
+            Assert.AreEqual(false, isModelStateValid);
+        }
+        [TestMethod]
+        public void EventModel_EmptyModelValidation()
+        {
+            var sut = new EventModel();
+            // Set some properties here
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            // Assert here
+            Assert.AreEqual(false, isModelStateValid);
+        }
+        // Participant Model Empty Model
+        [TestMethod]
+        public void ParticipantModel_EmptyModelValidation()
+        {
+            var sut = new ParticipantModel();
+            // Set some properties here
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            // Assert here
+            Assert.AreEqual(false, isModelStateValid);
+        }
+        [TestMethod]
+        public void ParticipantMdel_CorrectModelValidation()
+        {
+            var sut = new ParticipantModel()
+            {
+                Firstname = "name",
+                Familyname = "fname",
+                Idcode = "123",
+                Paymentmethod = "kaardimakse",
+                ParticipantType = "person",
+            };
+            // Set some properties here
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            // Assert here
+            Assert.AreEqual(true, isModelStateValid);
+        }
+        [TestMethod]
+        public void ParticipantMdel_CustomNumberAttributeValidation()
+        {
+            var sut = new ParticipantModel()
+            {
+                Firstname = "name",
+                Familyname = "fname",
+                Idcode = "wrong",
+                Paymentmethod = "kaardimakse",
+                ParticipantType = "person",
+            };
+            // Set some properties here
+            var context = new ValidationContext(sut, null, null);
+            var results = new List<ValidationResult>();
+            var isModelStateValid = Validator.TryValidateObject(sut, context, results, true);
+
+            // Assert here
+            Assert.AreEqual(false, isModelStateValid);
+        }
+    }
+}
